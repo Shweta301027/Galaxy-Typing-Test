965a1428240f632aed17b4902b0d03f9a950c85c
shweta luthra <shweta.luthra30@gmail.com>
2023-01-27 18:15:15 +0530

    second-coomit

---
+++ page/script.js
@@ -0,0 +1,14 @@
+const main =  document.querySelector(".box");
+const main2 = document.querySelector(".box2");
+const full = document.querySelector(".full");
+const menu = document.querySelector(".menu")
+
+// full.addEventListener('click', ()=>{
+//     main.classList.add('display_block');
+//     main2.classList.remove("display_block");
+// });
+
+// menu.addEventListener('click', ()=>{
+//     main.classList.remove('display_block');
+//     main2.classList.add("display_block");
+// });
