754aff8829e6adaee61be05b19046bed3fbd1175
shweta luthra <shweta.luthra30@gmail.com>
2023-01-29 13:05:07 +0530

    trying to make fall words

--- game play/script.js
+++ game play/script.js
@@ -1,22 +1,36 @@
 const button = document.querySelector(".StartGame");
 const gameContent = document.querySelector(".gameContent");
-const inputElement = document.querySelector("#InputWord");
 const scoreElement = document.querySelector(".score");
 const levelElement= document.querySelector(".level")
 
-//variables
-const  currentLevel = LEVEL;
-const gameWidth = gameContent.clientWidth;
-const gameHeight = gameContent.clientHeight;
-let score = 0;
-let gameOver = false;
-let  arrWords = [];
-let arrWordsDiv = [];
-let topVal = 0;
 
 
     const DICTIONARY = [
         'for','us','and','lie','love','hate','fate','fame','school','college','btc','elon','musk','courses','internet','patience','argentina','motivation','tech','info','send','mate', 'reactjs','game', 'brusca','graphic','copper','boca', 'lie', 'case','expand','absence','football','native', 'demon', 'thread', 'award', 'tycoon', 'riquelme','still','empirical','doll','java','ackerman','dinner','register','proof','script', 'wrist','sulphur','selection','slam','grandmother','assertive','eaux','javascript','admiration','recognize','roll','bank', 'reactor','gradient','ribbon','slayer','pleasant','path','draft','polish','art','hook'  
     ]
+  
+//  /     const word = generateRandomWord(DICTIONARY);
+//     arrWords.push(word);
+//     let wordDiv = document.createElement('div');
+//     wordDiv.innerHTML = `<p>${word}</p>`;
+//     wordDiv.classList.add('word');
+//     wordDiv.style.top = '-2px';
+//     wordDiv.style.zIndex = '1';
+//     arrWordsDiv.push(wordDiv);
+//     gameContent[0].appendChild(wordDiv);
+//   }
+  
+//   // GET RANDOM WORD FROM DICTIONARY
+//   function generateRandomWord(words) {
+//     return words[Math.floor(Math.random() * words.length)];
+//   }
+DICTIONARY.forEach(item=> console.log(item))
 
-   
+let arrWords = [];
+const word = (DICTIONARY);
+    arrWords.push(word);
+    let wordDiv = document.createElement('div');
+    wordDiv.innerHTML = `<p>${word}</p>`;
+    wordDiv.classList.add('word');
+
+    console.log(word)
