172724c098e4cb33bf89f958384b50be9c85055e
shweta luthra <shweta.luthra30@gmail.com>
2023-01-30 21:43:15 +0530

    added

--- game play/script.js
+++ game play/script.js
@@ -6,8 +6,9 @@ const inputElementID = document.getElementById('InputWord');
 
 
 
+
     const letter = [
-        'for','us','and','lie','love','hate','fate','fame','school','college','btc','elon','musk','courses','internet','patience','argentina','motivation','tech','info','send','mate', 'reactjs','game', 'brusca','graphic','copper','boca', 'lie', 'case','expand','absence','football','native', 'demon', 'thread', 'award', 'tycoon', 'riquelme','still','empirical','doll','java','ackerman','dinner','register','proof','script', 'wrist','sulphur','selection','slam','grandmother','assertive','eaux','javascript','admiration','recognize','roll','bank', 'reactor','gradient','ribbon','slayer','pleasant','path','draft','polish','art','hook']
+      'for','us','and','lie','love','hate','fate','fame','school','college','btc','elon','musk','courses','internet','patience','argentina','motivation','tech','info','send','mate', 'reactjs','game', 'brusca','graphic','copper','boca', 'lie', 'case','expand','absence','football','native', 'demon', 'thread', 'award', 'tycoon', 'riquelme','still','empirical','doll','java','ackerman','dinner','register','proof','script', 'wrist','sulphur','selection','slam','grandmother','assertive','eaux','javascript','admiration','recognize','roll','bank', 'reactor','gradient','ribbon','slayer','pleasant','path','draft','polish','art','hook'];
 
     let array=[] ;
     let arrWords = [];
@@ -15,23 +16,46 @@ const inputElementID = document.getElementById('InputWord');
     let score = 0;
     let gameOver = false
     let gameWidth = gameContent.clientWidth;
-    const gameHeight = gameContent.clientHeight;
-
 
     setInterval(() => {
-        const randomIndex = Math.floor(Math.random() * letter.length);
-        const randomWord = letter[randomIndex];
-        const randomLeft = Math.floor(Math.random() * gameWidth);
-        const randomDuration = Math.floor(Math.random() * 5000) + 5000;
+      const gameHeight = gameContent.clientHeight;
+      const randomIndex = Math.floor(Math.random() * letter.length);
+      const randomWord = letter[randomIndex];
+      const randomLeft = Math.floor(Math.random() * gameWidth);
+      const randomDuration = Math.floor(Math.random() * 5000) + 5000;
+      
+      const wordElement = document.createElement("div");
 
-        const wordElement = document.createElement("div");
+      const randomWordArr = randomWord.split("");
+
+      randomWordArr.forEach((letter)=>{
+        const  wordElementSpan = document.createElement("span");
+        wordElementSpan.innerText = letter;
+        wordElement.appendChild(wordElementSpan);
+      })
         wordElement.style.position = "absolute";
         wordElement.style.left = `${randomLeft}px`;
         wordElement.style.top = "-90px";
         wordElement.style.color = "white"
         wordElement.style.transition = `top ${randomDuration}ms linear`;
-        wordElement.innerText = randomWord;
+
         gameContent.appendChild(wordElement);
+    
+// to compare random word with input field
+
+        inputElementID.addEventListener("input",()=>{
+          const finalArr = wordElement.querySelectorAll("span");
+          const inputArr = inputElementID.value.split("");
+          //  console.log(finalArr);
+          // console.log(inputArr);
+          finalArr.forEach((charInSpan,index)=>{
+            if(charInSpan.innerText==inputArr[index]){
+                charInSpan.classList.add("hidden");
+            } 
+          })
+        })
+        inputElementID.value="";
+        
       
         setTimeout(() => {
           wordElement.style.top = `${gameHeight}px`;
@@ -40,57 +64,9 @@ const inputElementID = document.getElementById('InputWord');
         setTimeout(() => {
           wordElement.remove();
         }, randomDuration);
-      }, 5000);
-      
-      
- function getWord(){
-  let inputValue = inputElementID.value.split();
-  inputElementID.value = "";
-  if(randomWord.includes(inputValue)){
-    randomWord.classList.add("hidden")
-    updateScore();
-  }
- }
+      }, 5000)
 
- //update score
- function updateScore(){
-  score += 10;
-  scoreElement.innerHTML=`<p>Score${score}</p>`
- }
- 
-    // setTimeout(()=>{
-    //     for(let i=0; i<letter.length; i++){
-    //     gameContent.innerHTML = `<p>${letter[i]}</p>`;
-    //     gameContent.style.left = (Math.random() * (gameWidth - 150)).toString() + 'px';
-    //     }
-    // }, 1000)
-    
-    // console.log(letter[i], i)
-    // // console.log(gameContent)
-    
-    // generateRandomWord()
-  
-// })
-    
-    //     DICTIONARY.forEach((word)=>{
-    //         // console.log(word)
-    //       let wordDiv = document.createElement('div');
-    //        wordDiv.innerHTML = `<p>${word}</p>`;
-    //        wordDiv.classList.add('word');
-    //     //    console.log(wordDiv);
-    //     //    console.log(word);
-    //        gameContent.append(wordDiv);
-    //        wordDiv.style.left = (Math.random() * (gameWidth - 150)).toString() + 'px';
-    //        arrWords.push(wordDiv);
-    //     //    console.log(arrWords)
-    //        gameContent.appendChild(wordDiv);
-    // console.log(gameContent.innerText)
-    //        })
-        
-
-// function generateRandomWord(words) {
-//    words[Math.floor(Math.random() * words.length)];
-// console.log(words)
-// }
-
-
+      function updateScore() {
+        score += 10;
+        scoreElementID.innerHTML = `<p>Score ${score}</p>`;
+      }
